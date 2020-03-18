62a32ff0e55e59c395b971f76ffe675fdf5a096e
Florian Lacrampe <lacrampe.florian@gmail.com>
2020-03-18 21:57:25 +0100

    fix: debug

--- build.sh
+++ build.sh
@@ -7,11 +7,11 @@ cd docker-jitsi-meet
 
 JITSI_RELEASE=$1 JITSI_REPO=quadeare make
 
-JITSI_SERVICES="web prosody jicofo jvb"
+JITSI_SERVICES=(web prosody jicofo jvb)
 
 for service in "${JITSI_SERVICES[@]}"
 do
-	docker tag quadeare/${service} quadeare/jitsi-${service}
+    docker tag quadeare/${service} quadeare/jitsi-${service}
     docker tag quadeare/${service} quadeare/jitsi-${service}:${JITSI_BUILD}
     docker push quadeare/jitsi-${service}
     docker push quadeare/jitsi-${service}:${JITSI_BUILD}
