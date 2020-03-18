f3a99bef005858574a271713503527dad740b6bd
Florian Lacrampe <lacrampe.florian@gmail.com>
2020-03-18 21:00:59 +0100

    fix: debug

--- build.sh
+++ build.sh
@@ -7,3 +7,4 @@ cd docker-jitsi-meet
 
 JITSI_RELEASE=$1 JITSI_REPO=quadeare make
 JITSI_SERVICES="web prosody jicofo jvb" JITSI_RELEASE=$1 JITSI_REPO=quadeare make release
+JITSI_SERVICES="web prosody jicofo jvb" JITSI_RELEASE=$1 JITSI_BUILD=$1 JITSI_REPO=quadeare make release
