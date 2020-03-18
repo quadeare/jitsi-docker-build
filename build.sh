efc38cb423c164404a2880f48e2657bc7ae53862
Florian Lacrampe <lacrampe.florian@gmail.com>
2020-03-18 21:00:08 +0100

    fix: debug

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 cd jitsi
-JITSI_BUILD=$(cat version_$0)-$0
+JITSI_BUILD=$(cat version_$1)-$1
 
 cd docker-jitsi-meet
 
-JITSI_RELEASE=$0 JITSI_REPO=quadeare make
-JITSI_SERVICES="web prosody jicofo jvb" JITSI_RELEASE=$0 JITSI_REPO=quadeare make release
+JITSI_RELEASE=$1 JITSI_REPO=quadeare make
+JITSI_SERVICES="web prosody jicofo jvb" JITSI_RELEASE=$1 JITSI_REPO=quadeare make release
--- get_version.sh
+++ get_version.sh
@@ -2,8 +2,8 @@
 
 apt update && apt install -y sudo wget gnupg
 wget -qO - https://download.jitsi.org/jitsi-key.gpg.key | sudo apt-key add -
-sudo sh -c "echo 'deb https://download.jitsi.org $0/' > /etc/apt/sources.list.d/jitsi-$0.list"
+sudo sh -c "echo 'deb https://download.jitsi.org $1/' > /etc/apt/sources.list.d/jitsi-$1.list"
 apt-get -y update
 mkdir jitsi
-VERSION=$(apt-cache show jitsi-meet | grep Version | head -1) && echo ${VERSION//Version:} > jitsi/version_$0
-cat jitsi/version_$0
+VERSION=$(apt-cache show jitsi-meet | grep Version | head -1) && echo ${VERSION//Version:} > jitsi/version_$1
+cat jitsi/version_$1
