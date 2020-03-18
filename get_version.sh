0e5ae132409a522d2bc226944f30e45a24d210e1
Florian Lacrampe <lacrampe.florian@gmail.com>
2020-03-18 19:47:26 +0100

    fix: debug

---
+++ get_version.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+apt update && apt install -y sudo wget gnupg
+wget -qO - https://download.jitsi.org/jitsi-key.gpg.key | sudo apt-key add -
+sudo sh -c "echo 'deb https://download.jitsi.org testing/' > /etc/apt/sources.list.d/jitsi-testing.list"
+apt-get -y update
+VERSION=$(apt-cache show jitsi-meet | grep Version | head -1) && echo ${VERSION//Version:} > /root/version
+cat version
